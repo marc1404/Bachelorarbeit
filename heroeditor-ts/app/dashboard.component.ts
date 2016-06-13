9c2fb1503aa76d1235286b9f294e1b01771094cb
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-13 16:23:47 +0200

    Implement gotoDetail in dashboard

--- heroeditor-ts/app/dashboard.component.ts
+++ heroeditor-ts/app/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router-deprecated';
 
 @Component({
     selector: 'my-dashboard',
@@ -10,13 +11,17 @@ export class DashboardComponent implements OnInit {
 
     heroes:Hero[] = [];
 
-    constructor(private heroService:HeroService) {}
+    constructor(private heroService:HeroService, private router:Router) {}
 
     ngOnInit() {
         this.heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail() {}
+    gotoDetail(hero:Hero) {
+        let link = [ 'HeroDetail', { id: hero.id } ];
+
+        this.router.navigate(link);
+    }
 
 }
