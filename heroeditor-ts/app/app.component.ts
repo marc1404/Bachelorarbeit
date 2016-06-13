0a92567aad8608f97a4f767f0d750f63319a7971
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-13 16:01:51 +0200

    Add Dashboard Component to routing

--- heroeditor-ts/app/app.component.ts
+++ heroeditor-ts/app/app.component.ts
@@ -2,19 +2,28 @@ import { Component } from '@angular/core';
 import { HeroService } from './hero.service';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @RouteConfig([
     {
         path: '/heroes',
         name: 'Heroes',
         component: HeroesComponent
+    }, {
+       path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
     }
 ])
 @Component({
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <a [routerLink]="['Heroes']">Heroes</a>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
         <router-outlet></router-outlet>
     `,
     directives: [ROUTER_DIRECTIVES],
