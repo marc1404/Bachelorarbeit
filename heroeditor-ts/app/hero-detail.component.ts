f5736bec5319ed5d3277b70401665535b0638ca9
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-13 16:20:06 +0200

    Refactor template out of hero detail component

--- heroeditor-ts/app/hero-detail.component.ts
+++ heroeditor-ts/app/hero-detail.component.ts
@@ -5,17 +5,7 @@ import { HeroService } from './hero.service';
 
 @Component({
     selector: 'my-hero-detail',
-    template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-    <button (click)="goBack()">Back</button>
-  `
+    templateUrl: 'app/hero-detail.component.html'
 })
 export class HeroDetailComponent implements OnInit {
     hero: Hero;
