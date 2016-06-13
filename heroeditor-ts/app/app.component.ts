53ae6eae612c22635e15e541950d67271d6ab45d
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-13 14:46:48 +0200

    Fully implement hero detail component

--- heroeditor-ts/app/app.component.ts
+++ heroeditor-ts/app/app.component.ts
@@ -1,9 +1,9 @@
-import {Component} from '@angular/core';
-import {Hero} from './hero';
-
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 @Component({
     selector: 'my-app',
-    template: `
+    template:`
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
@@ -13,8 +13,9 @@ import {Hero} from './hero';
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
-    styles: [`
+    styles:[`
     .selected {
       background-color: #CFD8DC !important;
       color: white;
@@ -62,26 +63,24 @@ import {Hero} from './hero';
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+    directives: [HeroDetailComponent]
 })
 export class AppComponent {
     title = 'Tour of Heroes';
     heroes = HEROES;
-    selectedHero:Hero;
-
-    onSelect(hero:Hero) {
-        this.selectedHero = hero;
-    }
+    selectedHero: Hero;
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-var HEROES:Hero[] = [
-    {"id": 11, "name": "Mr. Nice"},
-    {"id": 12, "name": "Narco"},
-    {"id": 13, "name": "Bombasto"},
-    {"id": 14, "name": "Celeritas"},
-    {"id": 15, "name": "Magneta"},
-    {"id": 16, "name": "RubberMan"},
-    {"id": 17, "name": "Dynama"},
-    {"id": 18, "name": "Dr IQ"},
-    {"id": 19, "name": "Magma"},
-    {"id": 20, "name": "Tornado"}
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
 ];
