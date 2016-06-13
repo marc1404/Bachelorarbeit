c9b66d4a31529cb612a355e9c2c3c918a59558c6
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-13 15:08:02 +0200

    Use promises for mocked hero service

--- heroeditor-ts/app/app.component.ts
+++ heroeditor-ts/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+
 @Component({
     selector: 'my-app',
     template:`
@@ -64,11 +66,25 @@ import { HeroDetailComponent } from './hero-detail.component';
       border-radius: 4px 0 0 4px;
     }
   `],
-    directives: [HeroDetailComponent]
+    directives: [HeroDetailComponent],
+    providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
     title = 'Tour of Heroes';
     heroes: Hero[];
     selectedHero: Hero;
     onSelect(hero: Hero) { this.selectedHero = hero; }
+
+    constructor(private heroService: HeroService) {}
+
+    ngOnInit() {
+        this.getHeroes();
+    }
+
+    getHeroes() {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes);
+    }
+
 }
