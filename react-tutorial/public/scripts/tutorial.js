c9282651fc2fab9fa186ba07e534810f79d31084
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-15 11:45:34 +0200

    Add Comment react component

---
+++ react-tutorial/public/scripts/tutorial.js
@@ -0,0 +1,50 @@
+const CommentBox = React.createClass({
+  render() {
+    return (
+      <div class="commentBox">
+        <h1>Comments</h1>
+        <CommentList/>
+        <CommentForm/>
+      </div>
+    );
+  }
+});
+
+const CommentList = React.createClass({
+  render() {
+    return (
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+const CommentForm = React.createClass({
+  render() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+const Comment = React.createClass({
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox/>,
+  document.getElementById('content')
+);
