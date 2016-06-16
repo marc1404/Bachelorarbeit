57a4e4b1e977937a9c78899dbaabb87e0cc0cbd3
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-16 13:30:15 +0200

    Show mock contacts

--- react-contacts/app/Contacts.jsx
+++ react-contacts/app/Contacts.jsx
@@ -2,22 +2,36 @@ import React from 'react';
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import {List, ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+import contacts from './contacts-mock';
 
 export default class Contacts extends React.Component {
 
     render() {
+        const listItems = contacts.map(contact => {
+            return (
+                <ListItem key={contact.id}
+                          primaryText={contact.name}
+                          leftAvatar={<Avatar>{this.initials(contact.name)}</Avatar>}
+                />
+            );
+        });
+        
         return (
             <Card>
                 <CardHeader title="All contacts"/>
                 <CardText>
                     <List>
-                        <ListItem primaryText="Marc Vornetran"
-                                  leftAvatar={<Avatar>MV</Avatar>}
-                        />
+                        {listItems}
                     </List>
                 </CardText>
             </Card>
         );
+    }
+
+    initials(name) {
+        return name.split(' ').map(part => {
+            return part.charAt(0);
+        }).join('');
     }
 
 }
--- react-contacts/app/contacts-mock.js
+++ react-contacts/app/contacts-mock.js
@@ -1,4 +1,4 @@
 export default [
-    { name: 'Marc Vornetran', age: 22, email: 'marc@vornetran.de', phone: '+49 160 0815' },
-    { name: 'John Doe', age: 42, email: 'john.doe@gmail.com', phone: '+49 160 42000' }
+    { id: 0, name: 'Marc Vornetran', age: 22, email: 'marc@vornetran.de', phone: '+49 160 0815' },
+    { id: 1, name: 'John Doe', age: 42, email: 'john.doe@gmail.com', phone: '+49 160 42000' }
 ];
