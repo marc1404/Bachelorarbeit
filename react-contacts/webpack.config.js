6c7d79ed01746d18a14343803730faad945da0fb
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-16 10:59:32 +0200

    Setup react with webpack

--- react-contacts/webpack.config.js
+++ react-contacts/webpack.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    entry: 'app/main.js',
+    entry: './app/main.js',
     output: {
         path: __dirname,
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [
             {
                 test: /.jsx?$/,
-                loader: 'babel-loader',
+                loader: 'babel-loader?cacheDirectory',
                 exclude: /node_modules/
             }
         ]
