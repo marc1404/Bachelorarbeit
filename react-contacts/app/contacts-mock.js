3ee7a859ec4a8119ecdecaa43a26d688c9a1b1a2
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-16 13:23:03 +0200

    mock contacts

---
+++ react-contacts/app/contacts-mock.js
@@ -0,0 +1,4 @@
+export default [
+    { name: 'Marc Vornetran', age: 22, email: 'marc@vornetran.de', phone: '+49 160 0815' },
+    { name: 'John Doe', age: 42, email: 'john.doe@gmail.com', phone: '+49 160 42000' }
+];
