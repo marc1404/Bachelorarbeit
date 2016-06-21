690b3a54d82a719709eedf5ed0d0033ab00aacdc
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-21 15:38:39 +0200

    Downgrade phone service to ng1 service

--- angular-phonecat/app/main.ts
+++ angular-phonecat/app/main.ts
@@ -1,7 +1,12 @@
 import {UpgradeAdapter} from '@angular/upgrade';
 import {HTTP_PROVIDERS} from '@angular/http';
+import {Phone} from './core/phone/phone.service';
 
 const upgradeAdapter = new UpgradeAdapter();
 
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
 upgradeAdapter.addProvider(HTTP_PROVIDERS);
+upgradeAdapter.addProvider(Phone);
+
+angular.module('core.phone')
+    .factory('phone', upgradeAdapter.downgradeNg2Provider(Phone));
