2d03e09943905f96e9db56edd16e9f6d91da8bf2
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-21 14:39:53 +0200

    Add Angular2 Http Providers

--- angular-phonecat/app/main.ts
+++ angular-phonecat/app/main.ts
@@ -1,5 +1,7 @@
 import {UpgradeAdapter} from '@angular/upgrade';
+import {HTTP_PROVIDERS} from '@angular/http';
 
 const upgradeAdapter = new UpgradeAdapter();
 
 upgradeAdapter.bootstrap(document.documentElement, ['phonecatApp']);
+upgradeAdapter.addProvider(HTTP_PROVIDERS);
