ce7a6bd2baeed3f33a1ed9f5ca48323450b58ad2
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-21 15:33:30 +0200

    Add rxjs map operator

--- angular-phonecat/app/core/phone/phone.service.ts
+++ angular-phonecat/app/core/phone/phone.service.ts
@@ -3,6 +3,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
 
 export interface PhoneData {
     name:string;
