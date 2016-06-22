84399664bd30b58d56ca90e888b109973314c627
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-22 14:09:30 +0200

    Use Phone in phone list component

--- angular-phonecat/app/phone-list/phone-list.component.ts
+++ angular-phonecat/app/phone-list/phone-list.component.ts
@@ -1,15 +1,19 @@
 'use strict';
 
+import {Phone, PhoneData} from '../core/phone/phone.service';
+
 class PhoneListController {
 
-  phones: any[];
+  phones: PhoneData[];
   orderProp: string;
-  query: string;
 
-  static $inject = ['Phone'];
+  static $inject = ['phone'];
 
-  constructor(Phone: any) {
-    this.phones = Phone.query();
+  constructor(phone: Phone) {
+    phone.query().subscribe(phones => {
+      this.phones = phones;
+    });
+
     this.orderProp = 'age';
   }
 
