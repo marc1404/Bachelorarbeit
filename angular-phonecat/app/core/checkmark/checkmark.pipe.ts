b7ed8f3172d380044b7608b62bf8071980adc845
Marc Vornetran <marc.vornetran@gmail.com>
2016-06-27 15:26:26 +0200

    Make Checkmark pipe for angular2

---
+++ angular-phonecat/app/core/checkmark/checkmark.pipe.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'checkmark'})
+export class CheckmarkPipe implements PipeTransform {
+
+    transform(input:boolean) {
+        return input ? '\u2713' : '\u2718';
+    }
+
+}
